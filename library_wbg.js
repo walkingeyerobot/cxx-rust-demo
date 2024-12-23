2b1aa680c6de177548d9b827482aa9c83f7ea26d
Mitch Foley <mitch@thefoley.net>
2024-12-23 17:15:15 -0500

    clean up library_wbg

--- library_wbg.js
+++ library_wbg.js
@@ -9,30 +9,23 @@ var LibraryWbg = {
     table.set(offset + 3, false);
     ;
   },
-  __wbindgen_throw: (arg0, arg1) => {
-    // TODO(walkingeyerobot): move the helper functions out.
+  __wbindgen_throw: (function() {
+    // TODO(walkingeyerobot): how badly does this need ignoreBOM and fatal?
     const cachedTextDecoder = (typeof TextDecoder !== 'undefined' ? new TextDecoder('utf-8', { ignoreBOM: true, fatal: true }) : { decode: () => { throw Error('TextDecoder not available') } } );
 
     if (typeof TextDecoder !== 'undefined') {
       cachedTextDecoder.decode();
     };
 
-    let cachedUint8Memory0 = HEAP8;
-
-    function getUint8Memory0() {
-      return HEAP8;
-      if (cachedUint8Memory0 === null || cachedUint8Memory0.byteLength === 0) {
-        cachedUint8Memory0 = new Uint8Array(wasm.memory.buffer);
-      }
-      return cachedUint8Memory0;
-    }
-
     function getStringFromWasm0(ptr, len) {
       ptr = ptr >>> 0;
-      return cachedTextDecoder.decode(getUint8Memory0().subarray(ptr, ptr + len));
+      return cachedTextDecoder.decode(HEAP8.subarray(ptr, ptr + len));
     }
-    throw new Error(getStringFromWasm0(arg0, arg1));
-  },
+
+    return function(arg0, arg1) {
+      throw new Error(getStringFromWasm0(arg0, arg1));
+    };
+  }()),
 };
 
 addToLibrary(LibraryWbg);
