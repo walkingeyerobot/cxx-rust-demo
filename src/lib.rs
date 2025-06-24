5c33f9ecb370967f788c39f7a8b0b9a596f107fd
Mitch Foley <mitch@thefoley.net>
2025-06-24 18:11:53 -0400

    update example a little, upgrade rand

--- src/lib.rs
+++ src/lib.rs
@@ -1,55 +1,54 @@
-use rand::Rng;
 use wasm_bindgen::prelude::*;
 
-#[no_mangle]
-pub extern "C" fn random_rs(lower: f64, upper: f64) -> f64 {
-    let mut rng = rand::thread_rng();
-    return rng.gen_range(lower..upper);
-}
+use rand::distr::Uniform;
+use rand::Rng;
 
 #[wasm_bindgen]
-pub fn rs_add(a: i32, b: i32) -> i32 {
-    return a + b;
-}
-
-#[wasm_bindgen]
-#[repr(C)]
-struct RsPoint {
+pub struct Point {
     x: f64,
     y: f64,
 }
 
 #[wasm_bindgen]
-impl RsPoint {
+impl Point {
+    #[wasm_bindgen(constructor)]
+    pub fn new(x: f64, y: f64) -> Point {
+        Point { x, y }
+    }
+
+    #[wasm_bindgen(js_name = getX)]
     pub fn get_x(&self) -> f64 {
         self.x
     }
+
+    #[wasm_bindgen(js_name = getY)]
     pub fn get_y(&self) -> f64 {
         self.y
     }
 }
 
 #[wasm_bindgen]
-#[repr(C)]
-struct RandomPointGeneratorRs {
-    left: f64,
-    top: f64,
-    right: f64,
-    bottom: f64,
+pub struct RandomPointGenerator {
+    generator: rand::prelude::ThreadRng,
+    distribution_x: Uniform<f64>,
+    distribution_y: Uniform<f64>,
 }
 
 #[wasm_bindgen]
-impl RandomPointGeneratorRs {
-    pub fn new(left: f64, top: f64, right: f64, bottom: f64) -> Self {
-        Self { left, top, right, bottom }
+impl RandomPointGenerator {
+    #[wasm_bindgen(constructor)]
+    pub fn new(left: f64, top: f64, right: f64, bottom: f64) -> RandomPointGenerator {
+        RandomPointGenerator {
+            generator: rand::rng(),
+            distribution_x: Uniform::new(left, right).unwrap(),
+            distribution_y: Uniform::new(top, bottom).unwrap(),
+        }
     }
 
-    pub fn get_random_point(&self) -> RsPoint {
-        RsPoint {
-            //x: unsafe { random_cc(self.left, self.right) },
-            //y: unsafe { random_cc(self.top, self.bottom) },
-            x: random_rs(self.left, self.right),
-            y: random_rs(self.top, self.bottom),
-        }
+    #[wasm_bindgen(js_name = getRandomPoint)]
+    pub fn get_random_point(&mut self) -> Point {
+        let x = self.generator.sample(self.distribution_x);
+        let y = self.generator.sample(self.distribution_y);
+        Point::new(x, y)
     }
 }
